fb0d2ae9b9b02eeb684598b7f114aaae70f23629
AlmondCheese <almondcheesepublic@gmail.com>
2023-12-08 16:56:09 +0200

    Add GetBounds Method to DynamicComponent class

--- UI/DynamicComponent.cs
+++ UI/DynamicComponent.cs
@@ -7,8 +7,7 @@ namespace ACL.UI
 {
     public abstract class DynamicComponent : Component
     {
-        public bool FixedUpdatesEnabled {get; set;} = true;
-        // Determines if DynamicComponent gets FixedUpdates.
+        public bool FixedUpdatesEnabled {get; set;} = true; // Determines if DynamicComponent gets FixedUpdates.
         internal bool IgnoreObjmap { get; set; } = true; // Determines if this Object uses a box for collisions, ignoring the Objmap link.
         internal bool AllowCollisions { get; set; } = true; // Determines if this Object registers collisions.
         internal string ObjmapLink { get; set; } = string.Empty; // Objmap linked to this Dynamic Component.
@@ -18,6 +17,33 @@ namespace ACL.UI
         protected DynamicComponent()
         {
             GameInstance.CurrentGameInstance.FixedUpdateEvent += FixedUpdate;
+        }
+
+        public Rectangle GetBounds() // Get component hitbox.
+        {
+            Rectangle boundingBox = Rectangle.Empty;
+
+            int rectangleCount = 0;
+
+            var properties = GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var property in properties)
+            {
+                if (property.PropertyType == typeof(Rectangle) && FixedUpdatesEnabled == true)
+                {
+                    rectangleCount++;
+                    Rectangle rectangle = (Rectangle)property.GetValue(this);
+                    if (rectangleCount > 1)
+                    {
+                        boundingBox = Rectangle.Union(boundingBox, rectangle);
+                    }
+                    else
+                    {
+                        boundingBox = rectangle;
+                    }
+                }
+            }
+            return boundingBox;
         }
     }
 }
